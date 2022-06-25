82bd4e28e6834bf2d53a46f0e0833c427efd6458
Sandrian W <sandrianw.work@gmail.com>
2022-06-25 16:39:54 +0700

    update

--- update from sans/main.cpp
+++ update from sans/main.cpp
@@ -108,54 +108,56 @@ public:
     //}
 
 
-    class Boards // makes another class called boards, will be used to initiate the game boards the user can see
+
+};
+
+class Boards // makes another class called boards, will be used to initiate the game boards the user can see
+{
+private:
+    const static int rows = 10; // makes constants called rows and columns set to 10; to make the game board matrix
+    const static int columns = 10;
+    char grid[rows][columns]; // makes an multidimensional array of data type 'char'
+public:
+    void MakeBoards() // declares a function to make the game boards
     {
-    private:
-        const static int rows = 10; // makes constants called rows and columns set to 10; to make the game board matrix
-        const static int columns = 10;
-        char grid[rows][columns]; // makes an multidimensional array of data type 'char'
-    public:
-        void MakeBoards() // declares a function to make the game boards
+        for (int x = 0; x < rows; x++) // starts a for loop
         {
-            for (int x = 0; x < rows; x++) // starts a for loop
+            for (int y = 0; y < columns; y++) // nested for loop
             {
-                for (int y = 0; y < columns; y++) // nested for loop
-                {
-                    grid[x][y] = '-'; // sets the element in the position to a '-'
-                }
+                grid[x][y] = '-'; // sets the element in the position to a '-'
             }
         }
+    }
 
-        void updateBoards(bool i, int x, int y) // makes a function to update the game boards, takes 3 paramaters. a boolean, and two integers
+    void updateBoards(bool i, int x, int y) // makes a function to update the game boards, takes 3 paramaters. a boolean, and two integers
+    {
+        int xPos = x; // sets two variables equal to the int values passed in
+        int yPos = y;
+
+        if (i == true) // checks if i is equal to 'true'
         {
-            int xPos = x; // sets two variables equal to the int values passed in
-            int yPos = y;
-
-            if (i == true) // checks if i is equal to 'true'
-            {
-                grid[xPos][yPos] = 'X'; // sets the element in the position to 'Y'
-            }
-            else
-            {
-                grid[xPos][yPos] = 'O'; // sets the element in the position to 'N'
-            }
+            grid[xPos][yPos] = 'X'; // sets the element in the position to 'Y'
         }
-
-        void PrintBoards() // makes a function to print the boards
+        else
         {
-            int amt = 0; // sets an int called amt to 0
-
-            cout << "  0 1 2 3 4 5 6 7 8 9" << endl; // prints the top line of coordinates
-            for (int x = 0; x < rows; x++) { // for loop
-                cout << amt << " "; // outputs the amount variable to the console + a space
-                amt++; // increments amt
-                for (int y = 0; y < columns; y++) { // nested for loop
-                    cout << grid[x][y] << " "; // outputs the element of the grid to the console, plus a space
-                }
-                cout << endl; // outputs an endline character
-            }
+            grid[xPos][yPos] = 'O'; // sets the element in the position to 'N'
         }
-    };
+    }
+
+    void PrintBoards() // makes a function to print the boards
+    {
+        int amt = 0; // sets an int called amt to 0
+
+        cout << "  0 1 2 3 4 5 6 7 8 9" << endl; // prints the top line of coordinates
+        for (int x = 0; x < rows; x++) { // for loop
+            cout << amt << " "; // outputs the amount variable to the console + a space
+            amt++; // increments amt
+            for (int y = 0; y < columns; y++) { // nested for loop
+                cout << grid[x][y] << " "; // outputs the element of the grid to the console, plus a space
+            }
+            cout << endl; // outputs an endline character
+        }
+    }
 };
 
 int main() {
@@ -311,4 +313,5 @@ int main() {
 
         return 0;
     }
+
 
